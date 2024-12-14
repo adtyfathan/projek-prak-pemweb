911411648aafee584070a7b3900f0318bbbfd45e
ahmad zaky <145886371+zakychalamet@users.noreply.github.com>
2024-12-14 22:24:03 +0700

    Update tours-main.php

--- public/html/tours-main.php
+++ public/html/tours-main.php
@@ -14,12 +14,12 @@
             <div class="frame-group">
                 <div class="frame-container">
                     <div class="material-symbolscall-outline-parent">
-                        <img class="material-symbolscall-outline-icon" alt="" src="asset/material-symbols_call-outline.png">
+                        <img class="material-symbolscall-outline-icon" alt="" src="../asset/material-symbols_call-outline.png">
 
                         <div class="div">+1 3756 29391234</div>
                     </div>
                     <div class="material-symbolscall-outline-parent">
-                        <img class="material-symbolscall-outline-icon" alt="" src="asset/material-symbols_call-outline.png">
+                        <img class="material-symbolscall-outline-icon" alt="" src="../asset/material-symbols_call-outline.png">
 
                         <div class="div">+1 3756 29391234</div>
                         <div class="div">/</div>
@@ -28,13 +28,13 @@
                 </div>
                 <div class="tailor-my-trip-parent">
                     <div class="tailor-my-trip">Tailor My Trip</div>
-                    <img class="weuiarrow-filled-icon" alt="" src="asset/weui_arrow-filled.png">
+                    <img class="weuiarrow-filled-icon" alt="" src="../asset/weui_arrow-filled.png">
 
                 </div>
             </div>
             <div class="frame-div">
                 <div class="material-symbolstravel-parent">
-                    <img class="material-symbolstravel-icon" alt="" src="asset/material-symbols_travel.png">
+                    <img class="material-symbolstravel-icon" alt="" src="../asset/material-symbols_travel.png">
 
                     <div class="cungkuo-tour">Cungkuo Tour</div>
                 </div>
@@ -42,12 +42,12 @@
                     <div class="frame-parent2">
                         <div class="china-tours-parent">
                             <div class="div">China Tours</div>
-                            <img class="icsharp-expand-more-icon" alt="" src="asset/ic_sharp-expand-more.png">
+                            <img class="icsharp-expand-more-icon" alt="" src="../asset/ic_sharp-expand-more.png">
 
                         </div>
                         <div class="china-tours-parent">
                             <div class="div">About Us</div>
-                            <img class="icsharp-expand-more-icon" alt="" src="asset/ic_sharp-expand-more.png">
+                            <img class="icsharp-expand-more-icon" alt="" src="../asset/ic_sharp-expand-more.png">
 
                         </div>
                         <div class="contact-us-wrapper">
@@ -57,7 +57,7 @@
                         </div>
                     </div>
                     <div class="icbaseline-search-parent">
-                        <img class="icsharp-expand-more-icon" alt="" src="asset/ic_baseline-search.png">
+                        <img class="icsharp-expand-more-icon" alt="" src="../asset/ic_baseline-search.png">
 
                         <div class="div">Search Tours</div>
                     </div>
@@ -314,7 +314,7 @@
             </div>
             <div class="weuiarrow-filled-parent">
                 <div class="weuiarrow-filled">
-                    <img class="vector-icon" alt="" src="asset/Vector_left.png">
+                    <img class="vector-icon" alt="" src="../asset/Vector_left.png">
 
                 </div>
                 <div class="frame-parent24">
@@ -407,7 +407,7 @@
                     </div>
                 </div>
                 <div class="weuiarrow-filled1">
-                    <img class="vector-icon1" alt="" src="asset/Vector_right.png">
+                    <img class="vector-icon1" alt="" src="../asset/Vector_right.png">
 
                 </div>
             </div>
@@ -477,7 +477,7 @@
                     <div class="frame-parent40">
                         <div class="dashiconsadmin-users-parent">
                             <div class="dashiconsadmin-users">
-                                <img class="vector-icon2" alt="" src="asset/Vector.png">
+                                <img class="vector-icon2" alt="" src="../asset/Vector.png">
 
                             </div>
                             <div class="kevin-yang-wrapper">
@@ -488,7 +488,7 @@
                         </div>
                         <div class="dashiconsadmin-users-parent">
                             <div class="dashiconsadmin-users">
-                                <img class="mingcutetask-2-fill-icon" alt="" src="asset/mingcute_task-2-fill.png">
+                                <img class="mingcutetask-2-fill-icon" alt="" src="../asset/mingcute_task-2-fill.png">
 
                             </div>
                             <div class="kevin-yang-wrapper">
@@ -499,7 +499,7 @@
                         </div>
                         <div class="dashiconsadmin-users-parent">
                             <div class="dashiconsadmin-users">
-                                <img class="mingcutetask-2-fill-icon" alt="" src="asset/material-symbols-light_reviews.png">
+                                <img class="mingcutetask-2-fill-icon" alt="" src="../asset/material-symbols-light_reviews.png">
 
                             </div>
                             <div class="kevin-yang-wrapper">
@@ -510,7 +510,7 @@
                         </div>
                         <div class="dashiconsadmin-users-parent">
                             <div class="dashiconsadmin-users">
-                                <img class="mingcutetask-2-fill-icon" alt="" src="asset/tabler_beach.png">
+                                <img class="mingcutetask-2-fill-icon" alt="" src="../asset/tabler_beach.png">
 
                             </div>
                             <div class="kevin-yang-wrapper">
@@ -521,7 +521,7 @@
                         </div>
                         <div class="dashiconsadmin-users-parent">
                             <div class="dashiconsadmin-users">
-                                <img class="mingcutetask-2-fill-icon" alt="" src="asset/mdi_heart.png">
+                                <img class="mingcutetask-2-fill-icon" alt="" src="../asset/mdi_heart.png">
 
                             </div>
                             <div class="kevin-yang-wrapper">
@@ -541,7 +541,7 @@
                         your inbox.</div>
                 </div>
                 <div class="material-symbolsmail-outline">
-                    <img class="vector-icon3" alt="" src="asset/material-symbols_mail-outline.png">
+                    <img class="vector-icon3" alt="" src="../asset/material-symbols_mail-outline.png">
 
                 </div>
                 <div class="frame-parent42">
@@ -606,7 +606,7 @@
                                 </div>
                             </div>
                             <div class="tablerclock-filled-parent">
-                                <img class="tablerclock-filled-icon" alt="" src="asset/tabler_clock-filled.png">
+                                <img class="tablerclock-filled-icon" alt="" src="../asset/tabler_clock-filled.png">
 
                                 <div class="were-at-your-service-parent">
                                     <b class="best-china-small">We’re at your service</b>
@@ -620,19 +620,19 @@
                         </div>
                         <div class="logo-parent">
                             <div class="logo">
-                                <img class="vector-icon4" alt="" src="asset/youtube.png">
+                                <img class="vector-icon4" alt="" src="../asset/youtube.png">
 
                             </div>
                             <div class="logo">
-                                <img class="icbaseline-facebook-icon" alt="" src="asset/facebook.png">
+                                <img class="icbaseline-facebook-icon" alt="" src="../asset/facebook.png">
 
                             </div>
                             <div class="logo">
-                                <img class="icbaseline-facebook-icon" alt="" src="asset/twitter.png">
+                                <img class="icbaseline-facebook-icon" alt="" src="../asset/twitter.png">
 
                             </div>
                             <div class="logo">
-                                <img class="icbaseline-facebook-icon" alt="" src="asset/instagram.png">
+                                <img class="icbaseline-facebook-icon" alt="" src="../asset/instagram.png">
 
                             </div>
                         </div>
