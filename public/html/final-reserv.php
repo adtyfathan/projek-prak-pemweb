22c4a052450ab31100f97323aa603a4d72627eec
ahmad zaky <145886371+zakychalamet@users.noreply.github.com>
2024-12-15 17:40:53 +0700

    Create final-reserv.php

---
+++ public/html/final-reserv.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+<head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="initial-scale=1, width=device-width">
+  	<title>Cungkuo Tour</title>
+	<link rel="icon" href="../asset/favicon.png">
+  	<link rel="stylesheet"  href="../css/final-reserv.css" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" />
+</head>
+<body>
+  	<div class="final-reserv">
+    		<div class="frame-parent">
+      			<div class="frame-group">
+        				<div class="frame-container">
+          					<div class="material-symbolscall-outline-parent">
+            						<img class="material-symbolscall-outline-icon" alt="" src="../asset/material-symbols_call-outline.png">
+            						
+            						<div class="div">+1 3756 29391234</div>
+          					</div>
+          					<div class="material-symbolscall-outline-parent">
+            						<img class="material-symbolscall-outline-icon" alt="" src="../asset/material-symbols_call-outline.png">
+            						
+            						<div class="div">+1 3756 29391234</div>
+            						<div class="div">/</div>
+            						<div class="div">+1 3756 29391234</div>
+          					</div>
+        				</div>
+        				<div class="tailor-my-trip-parent">
+          					<div class="tailor-my-trip">Tailor My Trip</div>
+          					<img class="weuiarrow-filled-icon" alt="" src="../asset/weui_arrow-filled.png">
+          					
+        				</div>
+      			</div>
+      			<div class="frame-div">
+        				<div class="material-symbolstravel-parent">
+          					<img class="material-symbolstravel-icon" alt="" src="../asset/material-symbols_travel.png">
+          					
+          					<div class="cungkuo-tour">Cungkuo Tour</div>
+        				</div>
+        				<div class="frame-parent1">
+          					<div class="frame-parent2">
+								<div class="china-tours-parent">
+									<div class="div">China Tours</div>
+									<img class="icsharp-expand-more-icon" alt="" src="../asset/ic_sharp-expand-more.png">
+									<div class="dropdown_menu">
+										<div class="dropdown-columns">
+											<div class="dropdown-column">
+												<a href="../html/home.php">Best China Tours</a>
+												<a href="../html/tours-main.php">Senior Friendly Tours</a>
+												<a href="../html/top-10.php">Top 4 Best China Tours</a>
+												<a href="../html/1-week.php">1 Week Tours</a>
+											</div>
+											<div class="dropdown-column">
+												<a href="../html/tour-package.php">China Tour Package</a>
+												<a href="../html/student-tour.php">China Tour for Students</a>
+												<a href="../html/home.php">1st Time Travel China</a>
+												<a href="../html/theme-tour.php">China Theme</a>
+											</div>
+										</div>
+										<div class="dropdown-button">
+											<a href="#" class="all-theme-tours-button">All Theme Tours</a>
+										</div>
+									</div>
+								</div>
+            						<div class="china-tours-parent">
+              							<div class="div">About Us</div>
+              							<img class="icsharp-expand-more-icon" alt="" src="../asset/ic_sharp-expand-more.png">
+              							
+            						</div>
+            						<div class="contact-us-wrapper">
+              							<div class="div">Contact Us</div>
+            						</div>
+          					</div>
+          					<div class="icbaseline-search-parent">
+            						<img class="icsharp-expand-more-icon" alt="" src="../asset/ic_baseline-search.png">
+            						
+            						<div class="div">Search Tours</div>
+          					</div>
+        				</div>
+      			</div>
+    		</div>
+    		<div class="home-china">Home &gt; China Tour Packages &gt; Student Holiday</div>
+    		<div class="youre-interested-in-container">
+      			<p class="youre-interested-in">You're interested in the itinerary:</p>
+      			<p class="beijing-xian">Beijing, Xi'an & Shanghai Best Tour, 8 Days</p>
+      			<p class="beijing-xian">&nbsp;</p>
+      			<p class="youre-interested-in">One of our travel specialists will customize an itinerary for you within 24 hours. Tailor-made service is free of charge and we will work together with you until every detail is satisfied by you.</p>
+    		</div>
+    		<div class="frame-parent3">
+      			<div class="frame-wrapper">
+        				<div class="thannk-you-wrapper">
+          					<div class="div">Thank You!</div>
+        				</div>
+      			</div>
+      			<div class="frame-parent4">
+        				<div class="send-inquiry-wrapper">
+          					<div class="cungkuo-tour">Send Inquiry</div>
+        				</div>
+        				<div class="your-reservation-hase">Your Reservation Hase been record! Check Your email for detail. We can’t wait to meet you</div>
+      			</div>
+    		</div>
+    		<div class="final-reserv-inner">
+      			<div class="frame-wrapper1">
+        				<div class="frame-parent5">
+          					<div class="frame-parent6">
+            						<div class="frame-parent7">
+              							<div class="hot-destinations-parent">
+                								<b class="hot-destinations">HOT DESTINATIONS</b>
+                								<div class="frame-parent8">
+                  									<div class="beijing-parent">
+                    										<div class="beijing">Beijing</div>
+                    										<div class="beijing">Shanghai</div>
+                    										<div class="beijing">Chengdu</div>
+                    										<div class="beijing">Hangzou</div>
+                    										<div class="beijing">Xi’an</div>
+                  									</div>
+                  									<div class="beijing-parent">
+                    										<div class="beijing">Beijing</div>
+                    										<div class="beijing">Shanghai</div>
+                    										<div class="beijing">Chengdu</div>
+                    										<div class="beijing">Hangzou</div>
+                    										<div class="beijing">Xi’an</div>
+                  									</div>
+                								</div>
+              							</div>
+              							<div class="company-parent">
+                								<b class="hot-destinations">COMPANY</b>
+                								<div class="frame-wrapper2">
+                  									<div class="beijing-parent">
+                    										<div class="beijing">About Us</div>
+                    										<div class="review-by-clients">Review by Clients</div>
+                    										<div class="review-by-clients">Our Awards</div>
+                    										<div class="review-by-clients">Service Overview</div>
+                    										<div class="review-by-clients">Contact Us</div>
+                  									</div>
+                								</div>
+              							</div>
+              							<div class="resource-parent">
+                								<b class="hot-destinations">RESOURCE</b>
+                								<div class="frame-wrapper2">
+                  									<div class="beijing-parent">
+                    										<div class="customer-center">Customer Center</div>
+                    										<div class="review-by-clients">Payment Guide</div>
+                    										<div class="review-by-clients">Terms & Conditions</div>
+                    										<div class="review-by-clients">Cancellation & Refund</div>
+                    										<div class="review-by-clients">Feedback & Complaints</div>
+                  									</div>
+                								</div>
+              							</div>
+            						</div>
+            						<div class="tablerclock-filled-parent">
+              							<img class="tablerclock-filled-icon" alt="" src="../asset/tabler_clock-filled.png">
+              							
+              							<div class="were-at-your-service-parent">
+                								<b class="hot-destinations">We’re at your service</b>
+                								<div class="parent">
+                  									<div class="hot-destinations">+1 273 83492332</div>
+                  									<div class="hot-destinations">+61 2 934840234</div>
+                  									<div class="hot-destinations">+61 234 842 921</div>
+                								</div>
+              							</div>
+            						</div>
+          					</div>
+          					<div class="logo-parent">
+            						<div class="logo">
+              							<img class="vector-icon" alt="" src="../asset/youtube.png">
+              							
+            						</div>
+            						<div class="logo">
+              							<img class="icbaseline-facebook-icon" alt="" src="../asset/facebook.png">
+              							
+            						</div>
+            						<div class="logo">
+              							<img class="icbaseline-facebook-icon" alt="" src="../asset/twitter.png">
+              							
+            						</div>
+            						<div class="logo">
+              							<img class="icbaseline-facebook-icon" alt="" src="../asset/instagram.png">
+              							
+            						</div>
+          					</div>
+        				</div>
+      			</div>
+    		</div>
+    		<div class="frame-parent9">
+      			<div class="the-beauty-of-china-parent">
+        				<div class="hot-destinations">The Beauty of China</div>
+        				<div class="whether-you-are">Whether you are planning a family vacation or a senior tour, our travel specialists will customize the trip to perfectly match your interests and budget. Reach out to us, and let China Odyssey Tours bring your dream trip to life!</div>
+      			</div>
+      			<div class="group-parent">
+        				<div class="group-wrapper">
+          					<div class="image-7-parent">
+            						<img class="image-7-icon" alt="" src="../asset/e.jpg">
+            						</div>
+        				</div>
+        				<div class="group-wrapper">
+          					<div class="image-7-parent">
+            						<img class="image-7-icon" alt="" src="../asset/d.jpg">
+            						</div>
+        				</div>
+        				<div class="group-wrapper">
+          					<div class="image-7-parent">
+            						<img class="image-7-icon" alt="" src="../asset/b.jpg">
+            						</div>
+        				</div>
+        				<div class="group-wrapper">
+          					<div class="image-7-parent">
+            						<img class="image-7-icon" alt="" src="../asset/g.jpg">
+            						</div>
+        				</div>
+      			</div>
+      			<div class="group-parent">
+        				<div class="group-wrapper">
+          					<div class="image-7-parent">
+            						<img class="image-7-icon" alt="" src="../asset/f.jpg">
+            						</div>
+        				</div>
+        				<div class="group-wrapper">
+          					<div class="image-7-parent">
+            						<img class="image-7-icon" alt="" src="../asset/h.jpg">
+            						</div>
+        				</div>
+        				<div class="group-wrapper">
+          					<div class="image-7-parent">
+            						<img class="image-7-icon" alt="" src="../asset/a.jpg">
+            						</div>
+        				</div>
+        				<div class="group-wrapper">
+          					<div class="image-7-parent">
+            						<img class="image-7-icon" alt="" src="../asset/c626f6ff63c6b7c3594b694fe5ebcc85.jpg">
+            						</div>
+        				</div>
+      			</div>
+    		</div>
+  	</div>
+</body>
+</html>
